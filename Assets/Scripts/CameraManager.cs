a33586aa0d093d77da7719175db12db3f49580b6
flankids <flankids.sakura37@gmail.com>
2023-07-18 08:51:56 +0900

    第２章 課題① - 対策② カメラのイージング的な追従

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -7,6 +7,12 @@ public class CameraManager : MonoBehaviour {
     /// <summary>カメラと注視対象の相対的な座標</summary>
     private Vector3 _offsetPosition;
 
+    /// <summary>注視対象を追いかける最大速度(m/s)</summary>
+    [SerializeField] private float _maxSpeed = 10f;
+
+    /// <summary>注視対象を追いかける速度が最大になる距離</summary>
+    [SerializeField] private float _maxSpeedDistance = 2f;
+
     /// <summary>起動時の処理</summary>
     private void Awake() {
         if (_target == null) {
@@ -23,7 +29,14 @@ public class CameraManager : MonoBehaviour {
             return;
         }
 
-        // ゲーム開始時の位置関係を維持してカメラを追従させる
-        transform.position = _target.position + _offsetPosition;
+        var currentPos = transform.position;
+        // 移動目標座標
+        var targetPos = _target.position + _offsetPosition;
+        // カメラ座標と目標座標の距離
+        var distance = Vector3.Distance(currentPos, targetPos);
+        // 今回のフレームでのカメラの移動量
+        var maxDistanceDelta = _maxSpeed * Mathf.InverseLerp(0f, _maxSpeedDistance, distance) * Time.deltaTime;
+        // カメラを目標座標に向かって、計算した移動量分だけ移動させる
+        transform.position = Vector3.MoveTowards(currentPos, targetPos, maxDistanceDelta);
     }
 }
