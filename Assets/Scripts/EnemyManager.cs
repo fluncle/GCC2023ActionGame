c01b95e31f66ab2ad78657f3c5a93b8eccacbde1
川辺 兼嗣 <kawanabe_kenji@grenge.co.jp>
2023-07-18 09:06:08 +0900

    第２章 課題② - 対策① 攻撃のたびに敵に向かって回転させる

--- Assets/Scripts/EnemyManager.cs
+++ Assets/Scripts/EnemyManager.cs
@@ -34,4 +34,32 @@ public class EnemyManager : MonoBehaviour {
             GameUIManager.Instance.GameEndView.Play(true);
         }
     }
+
+    /// <summary>最寄りの敵を取得する</summary>
+    /// <param name="searchCenter">探査中心座標</param>
+    /// <param name="range">探査範囲</param>
+    public Enemy GetNearestEnemy(Vector3 searchCenter, float range) {
+        // 最寄りの敵を格納するローカル変数
+        Enemy nearestEnemy = null;
+        // 最寄りの敵との距離を格納するローカル変数
+        float nearestDiatance = float.MaxValue;
+        
+        foreach (var enemy in _enemies) {
+            // 敵と探査中心の距離を計算
+            var distance = Vector3.Distance(enemy.transform.position, searchCenter);
+            
+            // 距離が探査範囲外なら無視する
+            if (distance > range) {
+                continue;
+            }
+
+            // 計算した距離が、暫定の最寄り敵との距離より近ければ最寄りの敵情報を更新する
+            if (distance < nearestDiatance) {
+                nearestDiatance = distance;
+                nearestEnemy = enemy;
+            }
+        }
+
+        return nearestEnemy;
+    }
 }
