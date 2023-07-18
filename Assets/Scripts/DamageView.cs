c780a9a29b24d0ef5d235d87261f5c82b82b5a60
川辺 兼嗣 <kawanabe_kenji@grenge.co.jp>
2023-07-18 14:56:21 +0900

    第３章 課題① - 対策①' ダメージ量表示にアニメーションをつける

--- Assets/Scripts/DamageView.cs
+++ Assets/Scripts/DamageView.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class DamageView : MonoBehaviour {
     /// <summary>ダメージ量のテキスト</summary>
@@ -7,6 +8,9 @@ public class DamageView : MonoBehaviour {
 
     /// <summary>表示するワールド座標</summary>
     private Vector3 _playPosition;
+
+    /// <summary>移動アニメーションのRootオブジェクト</summary>
+    [SerializeField] private Transform _moveRoot; 
 
     /// <summary>再生</summary>
     /// <param name="damage">ダメージ量</param>
@@ -17,6 +21,13 @@ public class DamageView : MonoBehaviour {
         // 表示位置を更新
         _playPosition = worldPosition;
         Update();
+
+        // 上方向に対して±30°のランダムな角度のベクトルを取得
+        var moveVector = Quaternion.Euler(0f, 0f, Random.Range(-30, 30)) * Vector3.up;
+        // 表示を0.3秒で200px動かすアニメーションを作成・再生
+        DOTween.Sequence()
+            .SetLink(gameObject)
+            .Append(_moveRoot.DOLocalMove(moveVector * 200f, 0.3f).SetEase(Ease.OutCubic));
         
         // 0.5秒後に削除処理を呼ぶ
         Invoke(nameof(Destroy), 0.5f);
