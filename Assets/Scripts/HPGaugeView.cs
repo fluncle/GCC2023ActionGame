afa53cd4dd73e579060f0d6d90605ca13cd90a33
川辺 兼嗣 <kawanabe_kenji@grenge.co.jp>
2023-07-18 15:13:48 +0900

    第３章 課題① - 対策② HPゲージの差分表示

--- Assets/Scripts/HPGaugeView.cs
+++ Assets/Scripts/HPGaugeView.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class HPGaugeView : MonoBehaviour {
     /// <summary>ゲージのバー表示のImage</summary>
@@ -8,6 +9,11 @@ public class HPGaugeView : MonoBehaviour {
     /// <summary>HP量を表示するテキスト</summary>
     [SerializeField] private Text _valueText;
     
+    /// <summary>差分ゲージのバー表示のImage</summary>
+    [SerializeField] private Image _diffBar;
+
+    private Sequence _barSeq;
+
     /// <summary>最大HP</summary>
     private int _maxHP;
     
@@ -23,6 +29,13 @@ public class HPGaugeView : MonoBehaviour {
         var hpRate = (float)hp / _maxHP;
         // 残りHPの割合でバーの表示幅を更新
         _bar.fillAmount = hpRate;
+
+        _barSeq?.Kill();
+        _barSeq = DOTween.Sequence()
+            .SetLink(gameObject)
+            .SetDelay(0.75f)
+            .Append(_diffBar.DOFillAmount(hpRate, 0.5f));
+
         // HP量のテキストを更新
         _valueText.text = string.Format("HP {0}/{1}", hp, _maxHP);
     }
